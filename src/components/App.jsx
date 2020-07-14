c5f7f0d31f4cbbe01b97fab8956689e144221ceb
Yangfan Zhang <42492049+YangfanZhang@users.noreply.github.com>
2020-07-15 09:14:58 +1000

    Added functions to handle adding items in to do list app.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  function handleChange(event) {
+    setUserInput(event.target.value);
+  }
+  function handleClick() {
+    //   setList({ ...list, userInput });
+    setList(prevItems => {
+      return [...prevItems, userInput];
+    });
+    setUserInput("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleChange} value={userInput} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {list.map(todoItem => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
